bbcffd7e32f21572c2869e0c7080a0ae25dd7139
Riya Karki <riyakarki2308@gmail.com>
2024-08-09 12:24:18 +0530

    push to main

--- init/index.js
+++ init/index.js
@@ -3,14 +3,14 @@ const initData=require("./data.js");
 const Listing=require("../models/listing.js");
 
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+    await mongoose.connect('mongodb://127.0.0.1:27018/wanderlust');
 }
 
 main()
 .then(()=>{
     console.log("connected to mongoDb");
 }).catch((err)=>{
-    console.log(err);
+    console.log(err.message);
 })
 
 
